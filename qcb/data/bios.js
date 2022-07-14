f9c70aa44689def0bffb619f2c983029034130a4
William Hamilton <draxy80@gmail.com>
2022-07-14 19:19:00 +1000

    Updated widths on bios

--- qcb/data/bios.js
+++ qcb/data/bios.js
@@ -3,7 +3,7 @@ const bios_data = [
     photo: "/bios/bio_toddh.png",
     name: "Todd Hammond",
     description:
-      "Lorem ipsum dolar oakmdo asodmokm asodmkoasmd aosdmk asodkm oasdmk aoskdm oamsd omoewm g woegm woekgm wogme owegkm",
+      "Lorem ipsum dolar oakmdo asodmokm asodmkoasmd aosdmk asodkm oasdmk aoskdm oamsd omoewm g woegm woekgm wogme owegkm. Lorem ipsum dolar oakmdo asodmokm asodmkoasmd aosdmk asodkm oasdmk aoskdm oamsd omoewm g woegm woekgm wogme owegkm. Lorem ipsum dolar oakmdo asodmokm asodmkoasmd aosdmk asodkm oasdmk aoskdm oamsd omoewm g woegm woekgm wogme owegkm. Lorem ipsum dolar oakmdo asodmokm asodmkoasmd aosdmk asodkm oasdmk aoskdm oamsd omoewm g woegm woekgm wogme owegkm. Lorem ipsum dolar oakmdo asodmokm asodmkoasmd aosdmk asodkm oasdmk aoskdm oamsd omoewm g woegm woekgm wogme owegkm. Lorem ipsum dolar oakmdo asodmokm asodmkoasmd aosdmk asodkm oasdmk aoskdm oamsd omoewm g woegm woekgm wogme owegkm. ",
   },
   {
     photo: "/bios/bio_zacb.png",
