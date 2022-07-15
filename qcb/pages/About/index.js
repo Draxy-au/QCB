4764cb41313d5eb2faa4363669cd25383363e4f3
William Hamilton <draxy80@gmail.com>
2022-07-15 13:24:31 +1000

    Abouted About for QCB

--- qcb/pages/About/index.js
+++ qcb/pages/About/index.js
@@ -28,8 +28,9 @@ export default function index() {
       <div className={styles.title_1}>We Are One Family</div>
       <div className={styles.normal_text}>
         <p>
-          We here at QCB have a zero tolerance policy for anybody who brings an
-          attitude that belongs firmly in the past.
+          We here at QLD Camping Bears have a zero tolerance policy for anybody
+          who brings an attitude towards individuals that we believe belongs
+          firmly in the past.
         </p>
         <p>
           Any vilification of any person by age, gender, orientation, size,
@@ -38,26 +39,29 @@ export default function index() {
         </p>
         <p>
           Equality, acceptance and kindness should be the norm, not the
-          exception. As a group, we are a minority, we should never be putting
-          down others, but raising everyone up to a higher standard and being
-          respectful at all times.
+          exception. As a group, we are a minority, and therefore we should
+          never be putting down others, but raising everyone up to a higher
+          standard and being respectful at all times.
         </p>
         <p>
-          Under no circumstances are you to enter another persons property
-          (entering into someone’s tent, van, vehicle or cabin) without the
-          owners permission.
+          Under no circumstances are you to enter or access another persons
+          property (entering into someone’s tent, van, vehicle or cabin) without
+          the owners expressed permission.
         </p>
         <p>
           The Qld Camping Bears committee, as always, reserves the right to
-          discipline any member found breaching these guidelines as per the Qld
-          Camping Bears constitution. Any non-members be found to be breaching
-          the above may be asked to leave any of our private events.
+          revoke your membership at our discretion at any time if you are found
+          to be breaching these guidelines as per our terms and conditions.
+        </p>
+        <p>
+          Any guests or members found to be breaching the terms and conditions
+          may be asked to leave any of our private events and no refunds will be
+          offered for any funds received.
         </p>
         <p>
           If you ever feel uncomfortable at any of our events, please feel free
-          to see one of our committee members immediately, who will always be
-          wearing either a committee shirt or a committee badge and we will
-          address your concerns as best we can.
+          to see one of our committee members immediately, and we will address
+          your concerns as best we can.
         </p>
         <p>
           If you wish to submit any details via email or anonymously, you can do
