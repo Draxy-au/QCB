319c75abf8187f5b74915e18b9a04efb3f3481c6
William Hamilton <draxy80@gmail.com>
2022-07-16 16:05:49 +1000

    Added shop layout

--- qcb/src/pages/Shop/index.js
+++ qcb/src/pages/Shop/index.js
@@ -1,15 +1,24 @@
+import Image from "next/image";
+
 import Navbar from "@components/Navbar";
 
 import styles from "./Shop.module.scss";
 
 import products from "@data/products";
 
+import cart_icon from "@assets/icons/cart.svg";
+
 export const Shop = () => {
   return (
     <div className={styles.shoppage_container}>
       <Navbar current={"Shop"} />
       <div className={styles.title_1}>QCB Shop</div>
+
       <div className={styles.products_container}>
+        <div className={styles.cart}>
+          Total: $0.00
+          <Image height={"30px"} width={"30px"} src={cart_icon} alt="cart" />
+        </div>
         {products?.slice(0, 6).map((product) => (
           <div key={product.id} className={styles.product}>
             <div className={styles.product_image}>
