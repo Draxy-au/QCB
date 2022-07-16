a7d952a68882ab24262bd6c1134b75e997192995
William Hamilton <draxy80@gmail.com>
2022-07-16 22:00:06 +1000

    Added graphql apollo/client. Added Hero banner

--- qcb/src/pages/Shop/index.js
+++ qcb/src/pages/Shop/index.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
 import Navbar from "@components/Navbar";
 
@@ -7,12 +8,31 @@ import styles from "./Shop.module.scss";
 import products from "@data/products";
 
 import cart_icon from "@assets/icons/cart.svg";
+import Link from "next/link";
 
-export const Shop = () => {
+export default function Shop({ home }) {
+  const { heroTitle, heroText, heroLink, heroBackground } = home;
   return (
     <div className={styles.shoppage_container}>
       <Navbar current={"Shop"} />
-      <div className={styles.title_1}>QCB Shop</div>
+
+      <div className={styles.hero}>
+        <Link href={heroLink || ""}>
+          <a>
+            <div className={styles.heroContent}>
+              <h2>{heroTitle}</h2>
+              <p>{heroText}</p>
+            </div>
+            <img
+              className={styles.heroImage}
+              src={heroBackground.url}
+              alt={heroTitle}
+              height={heroBackground.height}
+              width={heroBackground.width}
+            />
+          </a>
+        </Link>
+      </div>
 
       <div className={styles.products_container}>
         <div className={styles.cart}>
@@ -39,6 +59,39 @@ export const Shop = () => {
       </div>
     </div>
   );
-};
+}
 
-export default Shop;
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    uri: "https://api-ap-southeast-2.hygraph.com/v2/cl5nm23h70znu01ugcgu20nyv/master",
+    cache: new InMemoryCache(),
+  });
+
+  const data = await client.query({
+    query: gql`
+      query PageHome {
+        page(where: { slug: "home" }) {
+          id
+          heroLink
+          heroText
+          heroTitle
+          name
+          slug
+          heroBackground {
+            url
+            width
+            height
+          }
+        }
+      }
+    `,
+  });
+
+  console.log("Data: ", data);
+  const home = data.data.page;
+  return {
+    props: {
+      home,
+    },
+  };
+}
