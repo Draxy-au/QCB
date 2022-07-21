c6f3f7322af142ac15a5748aeb73e76a9eda379c
William Hamilton <draxy80@gmail.com>
2022-07-21 16:49:01 +1000

    Added Events/CampingEssentials page

---
+++ qcb/src/pages/Events/CampingEssentials/index.js
@@ -0,0 +1,48 @@
+import Navbar from "@components/Navbar";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+
+import styles from "./CampingEssentials.module.scss";
+
+export default function CampingEssentials({ campingEssentials }) {
+  return (
+    <div className={styles.campingessentials_page_container}>
+      <Navbar current={"About Us"} />
+      <div
+        className="pages"
+        dangerouslySetInnerHTML={{
+          __html: campingEssentials.content.html,
+        }}
+      />
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    uri: "https://api-ap-southeast-2.hygraph.com/v2/cl5nm23h70znu01ugcgu20nyv/master",
+    cache: new InMemoryCache(),
+  });
+
+  const data = await client.query({
+    query: gql`
+      query PageCampingEssentials {
+        page(where: { slug: "camping-essentials" }) {
+          id
+          name
+          slug
+          content {
+            html
+          }
+        }
+      }
+    `,
+  });
+
+  const campingEssentials = data.data.page;
+
+  return {
+    props: {
+      campingEssentials,
+    },
+  };
+}
