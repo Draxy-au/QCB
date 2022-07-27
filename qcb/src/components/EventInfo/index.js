45f5cc468724bb6ae296a1d88b52c38b92bef72e
William Hamilton <draxy80@gmail.com>
2022-07-28 07:08:16 +1000

    Setting Verified member visibility

--- qcb/src/components/EventInfo/index.js
+++ qcb/src/components/EventInfo/index.js
@@ -18,8 +18,6 @@ export const EventInfo = ({ event, verifiedMember }) => {
         <div className={styles.date_time}>
           {event.date} {event.time}
         </div>
-        {<>VERIFIED MEMBER: {verifiedMember.toString()}</>}
-
         <div className={styles.duration}>
           <span className={styles.bold}>Duration:</span> {event.duration}{" "}
           {event.duration < 2 ? "day" : "days"}
@@ -28,25 +26,29 @@ export const EventInfo = ({ event, verifiedMember }) => {
           <span className={styles.bold}>Location:</span> {event.venue} :{" "}
           {event.venueAddress}
         </div>
-        <div className={styles.map}>
-          <Link
-            href={`https://maps.google.com/?q=${event.map.latitude},${event.map.longitude}`}
-          >
-            <a target="_blank">
-              <button className={styles.map_button}>Map</button>
-            </a>
-          </Link>
-        </div>
-        {event.indigenousLand && (
+        {verifiedMember && (
+          <div className={styles.map}>
+            <Link
+              href={`https://maps.google.com/?q=${event.map.latitude},${event.map.longitude}`}
+            >
+              <a target="_blank">
+                <button className={styles.map_button}>Map</button>
+              </a>
+            </Link>
+          </div>
+        )}
+        {verifiedMember && event.indigenousLand && (
           <div className={styles.indigenous_land}>
             <span className={styles.bold}>Indigenous Land:</span>{" "}
             {event.indigenousLand}
           </div>
         )}
-        <div className={styles.cost}>
-          <span className={styles.bold}>Cost:</span> {event.costDetails}
-        </div>
-        {event.ticketsLink && (
+        {verifiedMember && (
+          <div className={styles.cost}>
+            <span className={styles.bold}>Cost:</span> {event.costDetails}
+          </div>
+        )}
+        {verifiedMember && event.ticketsLink && (
           <div className={styles.ticket_link}>
             <Link href={event.ticketsLink}>
               <a target="_blank">
@@ -55,15 +57,17 @@ export const EventInfo = ({ event, verifiedMember }) => {
             </Link>
           </div>
         )}
-        <div className={styles.details}>
-          <span className={styles.bold}>Details:</span>
-          <div
-            dangerouslySetInnerHTML={{
-              __html: event.description.html,
-            }}
-          />
-        </div>
-        {event.facebookEventLink && (
+        {verifiedMember && (
+          <div className={styles.details}>
+            <span className={styles.bold}>Details:</span>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: event.description.html,
+              }}
+            />
+          </div>
+        )}
+        {verifiedMember && event.facebookEventLink && (
           <div className={styles.facebook_link}>
             <Link href={event.facebookEventLink}>
               <a target="_blank">
