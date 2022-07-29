e75510d08866963d2887f78e1ac48931decc39b7
William Hamilton <draxy80@gmail.com>
2022-07-29 18:13:33 +1000

    Added router to news post component

--- qcb/src/pages/News/[slug]/index.js
+++ qcb/src/pages/News/[slug]/index.js
@@ -4,8 +4,11 @@ import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 import styles from "./NewsPost.module.scss";
 import Navbar from "@components/Navbar";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 export default function NewsPost({ post }) {
+  const router = useRouter();
+
   const goBack = () => {
     router.push("/Members/Portal/Dashboard");
   };
